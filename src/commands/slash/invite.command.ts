88d4c0f62ac34f02a36d1bf22525e3d404657b72
Alfred-207 <64010636+Alfred-207@users.noreply.github.com>
2021-12-15 20:42:30 +0000

    feat[invite]: new command for level

--- src/commands/slash/invite.command.ts
+++ src/commands/slash/invite.command.ts
@@ -1,6 +1,7 @@
-import { Client, CommandInteraction } from "discord.js";
-import { CommandConfig, ResponseArgs } from "../../types.bot";
+import { Client, CommandInteraction, MessageEmbed } from "discord.js";
+import { CommandConfig, ResponseArgs, Database, CommandList } from "../../types.bot";
 import Command from "../../resources/classes/command.class";
+import BotConfig from "../../config.bot";
 
 const Config: CommandConfig = {
   name: "invite",
@@ -18,10 +19,26 @@ export default abstract class InviteCommand extends Command {
   async execute(
     client: Client,
     message: CommandInteraction,
-    args: ResponseArgs
+    args: ResponseArgs,
+    commandlist: CommandList,
+    db: Database
   ): Promise<void> {
-    message.reply(
-      `You can invite Giftcord here: **[Click me!](https://discord.com/api/oauth2/authorize?client_id=917133047583895552&permissions=412384357441&scope=bot%20applications.commands)**`
-    );
+    let dbUser = await db.users.findOne({ userId: message.user.id });
+    if (!dbUser) {
+      const noProfile = new MessageEmbed()
+        .setAuthor(message.user.tag, message.user?.avatarURL()?.toString())
+        .setColor(BotConfig.colors.Red)
+        .setDescription("Oh no! It seems as if you don't have a profile. Run the /profile command to make one!");
+      message.reply({embeds: [noProfile], ephemeral: true});
+    } else {
+    let embed = new MessageEmbed()
+      //.setTitle(`${message.user.username}'s Level`)
+      .setAuthor(`${message.user.username}'s`, message.user.displayAvatarURL())
+      .addField(`  Level  `, `${dbUser?.currentLevel}`, true)
+      .addField(`  XP  `, `${dbUser?.currentXp}/${dbUser?.xpRequired}`, true)
+      //.addField(`  Total XP  `, `${dbUser?.currentXp}`, true)
+      .setColor(BotConfig.colors.Main)
+    message.reply({embeds: [embed], ephemeral: true});
+    }
   }
 }
